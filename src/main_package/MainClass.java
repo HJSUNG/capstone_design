9af5e8175288948717a55b9ffcf7834fe3a7596a
HJSUNG <38451225+HJSUNG@users.noreply.github.com>
2018-09-09 14:47:42 +0900

    test commit

---
+++ src/main_package/MainClass.java
@@ -0,0 +1,12 @@
+package main_package;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test commit");
+		System.out.println("Capstone Design");
+		System.out.println("2018.9.9");
+	}
+
+}
