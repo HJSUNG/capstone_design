1e77f1b256b09673b73184ce28275ea566c91370
dlaalswjd <rladidtla70@naver.com>
2018-09-30 17:32:49 +0900

    Create List.java

---
+++ Client/app/src/main/java/csecau/capstone/homeseek/List.java
@@ -0,0 +1,13 @@
+package csecau.capstone.homeseek;
+
+public class List {
+    private String text;
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public String getText(){
+        return this.text;
+    }
+}
