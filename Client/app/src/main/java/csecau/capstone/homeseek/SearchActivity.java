ee59a59951143cf4b9254b9c5bf782461137cc91
dlaalswjd <rladidtla70@naver.com>
2018-09-30 17:33:31 +0900

    Create SearchActivity.java

---
+++ Client/app/src/main/java/csecau/capstone/homeseek/SearchActivity.java
@@ -0,0 +1,22 @@
+package csecau.capstone.homeseek;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        PageAdapter adapter = new PageAdapter(getSupportFragmentManager());
+        adapter.addFrag(new Grid_view(), "GridView");
+        viewPager.setAdapter(adapter);
+    }
+}
