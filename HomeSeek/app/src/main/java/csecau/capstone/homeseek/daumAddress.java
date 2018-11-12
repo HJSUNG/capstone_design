68290e038da78283e38c95852d7e090ba7864ceb
dlaalswjd <rladidtla70@naver.com>
2018-11-12 21:34:41 +0900

    upload postingActivity

--- HomeSeek/app/src/main/java/csecau/capstone/homeseek/daumAddress.java
+++ HomeSeek/app/src/main/java/csecau/capstone/homeseek/daumAddress.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -12,42 +13,43 @@ import android.os.Handler;
 public class daumAddress extends AppCompatActivity {
     private WebView browser;
     private TextView daum_result;
-    private final Handler handler = new Handler();
-
-    class MyJavaScriptInterface {
-        @JavascriptInterface
-        @SuppressWarnings("unused")
-        public void processDATA(String data) {
-
-            Bundle extra = new Bundle();
-            Intent intent = new Intent();
-            extra.putString("data", data);
-            intent.putExtras(extra);
-            setResult(RESULT_OK, intent);
-            finish();
-
-
-        }
-    }
+    private Handler handler;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daum);
-
-        browser = (WebView) findViewById(R.id.daum_webview);
         daum_result = (TextView)findViewById(R.id.daum_result);
+
+        init_webView();
+
+        handler = new Handler();
+    }
+
+    public void init_webView(){
+        browser = (WebView)findViewById(R.id.daum_webview);
         browser.getSettings().setJavaScriptEnabled(true);
         browser.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        browser.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
+        browser.addJavascriptInterface(new AndroidBridge(), "TestApp");
+        browser.setWebChromeClient(new WebChromeClient());
+        browser.loadUrl("http://dozonexx.dothome.co.kr/daum_address.php");
+    }
 
-        browser.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
+    private class AndroidBridge {
+        @JavascriptInterface
+        public void setAddress(final String arg1, final String arg2, final String arg3) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    daum_result.setText(String.format("%s %s", arg2, arg3));
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("data", daum_result.getText().toString());
+                    setResult(RESULT_OK, returnIntent);
+                    finish();
 
-                browser.loadUrl("javascript:sample2_execDaumPostcode();");
-            }
-        });
-
+                    init_webView();
+                }
+            });
+        }
     }
 }
