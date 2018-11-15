232905fce29157079b09c0999a0cf869d3166d18
dovicvi <38933683+dovicvi@users.noreply.github.com>
2018-11-15 18:04:27 +0900

    merge before

--- HomeSeek/app/src/main/java/csecau/capstone/homeseek/Picture_List_Activity.java
+++ HomeSeek/app/src/main/java/csecau/capstone/homeseek/Picture_List_Activity.java
@@ -27,16 +27,19 @@ public class Picture_List_Activity extends Activity {
                 new list_item(R.drawable.room_02,"흑석원룸","정문 앞 좋은 원룸이요","보증금 500에 월세 35 받습니다."));
 
         list_itemArrayList.add(
-                new list_item(R.drawable.room_03,"닉네임2","제목2","내용2"));
+                new list_item(R.drawable.room_03,"중앙대원룸","후문 코앞 원룸","후문 정류장 바로 윗집"));
 
         list_itemArrayList.add(
-                new list_item(R.drawable.room_04,"닉네임3","제목3", "내용3"));
+                new list_item(R.drawable.room_04,"중앙대원룸","후문 도보 5분거리", "호텔 같은 자취방"));
 
         list_itemArrayList.add(
-                new list_item(R.drawable.room_05,"닉네임4","제목4", "내용4"));
+                new list_item(R.drawable.room_05,"흑석원룸","신축 좋은 방 있습니다.", "신축이라 깨끗합니다."));
 
         list_itemArrayList.add(
-                new list_item(R.drawable.room_05,"닉네임","제목", "내용"));
+                new list_item(R.drawable.room_06,"중앙대자취방","신축원룸 월세,관리비포함 55요", "신축에 풀옵션이에요"));
+
+        list_itemArrayList.add(
+                new list_item(R.drawable.room_07,"중앙대자취방","신축원룸 전세로도 가능합니다.", "1000/60 또는 전세로 8000"));
 
 
         pictureListAdapter = new PictureListAdapter(Picture_List_Activity.this, list_itemArrayList);
